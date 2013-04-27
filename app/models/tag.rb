c127198aa98dc009e4ab65f2f813497218d60d2e
Alex Falke <afalkear@gmail.com>
2013-04-27 18:54:13 -0300

    tag attributes changed

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,7 @@
 class Tag < LogicalModel
   attr_accessor :name
 
-  self.attribute_keys = [:_id, :name, :contact_id, :account_id]
+  self.attribute_keys = [:_id, :name, :contact_ids, :account_name]
 
   self.hydra = Contacts::HYDRA
   self.resource_path = "/v0/tags"
@@ -28,30 +28,31 @@ class Tag < LogicalModel
     self._id = id
   end
 
+
   # Returns all the tags associated with a given account
-  def account_tags(account_name)
+  def self.account_tags(account_name)
     params = { account_name: account_name }
     params = self.merge_key(params)
 
 
     response = nil
     Timeout::timeout(self.timeout/1000) do
-      response = Typhoeus::Request.get( "/v0/accounts/#{account_name}"+"/tags", :params => params, :timeout => self.timeout )
+      response = Typhoeus::Request.get( "#{url_protocol_prefix}#{self.host}/v0/accounts/#{account_name}/tags", :params => params, :timeout => self.timeout )
     end
-    case response.code
-      when 200
-        log_ok response
-        return true
-      when 400
-        log_failed response
-        return false
-      else
-        log_failed response
-        return nil
-    end
-    rescue Timeout::Error
-      self.logger.warn "timeout"
+
+    if response.code == 200
+      log_ok(response)
+      result_set = self.from_json(response.body)
+      return result_set[:collection]
+    elsif response.code == 400
+      log_failed(response)
+      return false
+    else
+      log_failed(response)
       return nil
     end
+  rescue Timeout::Error
+    self.logger.warn "timeout"
+    return nil
   end
 end
